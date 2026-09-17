4a3d8072b24c179f3a1bb84ea0aaacf0d58659ad
MarselaBiak <marselabiak210@gmail.com>
2026-09-17 14:42:41 +0300

    dz22

--- js/methodsTasks.js
+++ js/methodsTasks.js
@@ -1,67 +1,48 @@
 'use strict';
 
-// задание 1
+const user = {
+    name: 'Anna',
+    age: 25,
+    status: 'student',
 
-const products = [
-    { id: 1, name: "Mouse", price: 25, inStock: true },
-    { id: 2, name: "Keyboard", price: 70, inStock: false },
-    { id: 3, name: "Monitor", price: 210, inStock: true },
-];
+    showName(city, country) {
+        return `${this.name}, ${city}, ${country}`
+    }
+}
 
-console.log(products);
+// bind 
 
-const result =  products.map(el  => `${el.name}: $${el.price}${el.inStock ? '' : ' (out of stock)'}`)
+const bind = (func, object, ...args)  => {
+    return function() {
+        object.temp = func
+        const result = object.temp(...args)
+        delete object.temp
+        return result
+    }
+}
 
-console.log(result);
+const newFunc = bind(user.showName, user, 'Odesa', 'Ukraine')
 
-// задание 2
+console.log(newFunc())
 
-const users = [
-    { id:  1, age: 17, active: true, email: "a@mail.com" },
-    { id: 2, age: 22, active: true, email: "b@spam.com" },
-    { id: 3, age: 30, active: false, email: "c@mail.com" },
-    { id: 4, age: 35, active: true, email: "d@mail.com" },
-    { id: 5, age: 40, active: true, email: "e@mail.com" },
-];
+// call
 
-console.log(users);
+const call = (func, object, ...args) => {
+    object.temp = func
+    const result = object.temp(...args)
+    delete object.temp
+    return result
+}
 
-const result2 = users.filter(el => el.active === true && (el.age >= 18 && el.age <= 35) && !el.email.endsWith('@spam.com'))
+console.log(call(user.showName, user, 'Odesa', 'Ukraine'))
 
-console.log(result2);
+// apply
 
-// задание 3
+const apply = (func, object, args) => {
+    object.temp = func
+    const result = object.temp(...args)
+    delete object.temp
+    return result
+}
 
-const tx = [
-    { id: 1, category: "food", amount: 12 },
-    { id: 2, category: "food", amount: 8 },
-    { id: 3, category: "taxi", amount: 15 },
-    { id: 4, category: "books", amount: 20 },
-    { id: 5, category: "taxi", amount: 7 },
-];
-
-console.log(tx);
-
-const result3 = tx.reduce((acc, el) => {
-    acc[el.category] = (acc[el.category] || 0) + el.amount;
-    return acc;
-}, {});
-
-console.log(result3);
-
-// задание 4
-
-const orders = [
-    { id: 101, items: [{ sku: "A1", qty: 1 }, { sku: "C3", qty: 2 }] },
-    { id: 102, items: [{ sku: "B2", qty: 1 }] },
-    { id: 103, items: [{ sku: "B2", qty: 3 }, { sku: "A1", qty: 1 }] },
-];
-
-const result4 = orders.find(el => el.items.some(item => item.sku === "B2"));
-
-console.log(result4);
-
-
-
-
-
+console.log(apply(user.showName, user, ['Odesa', 'Ukraine']))
